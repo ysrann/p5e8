2f6b1ebf13643fb896eed7321222bfabb0902203
ysrann <faiqyusran@gmail.com>
2025-05-17 09:34:22 +0700

    Create question.js

---
+++ question.js
@@ -0,0 +1,52 @@
+const questions = [
+  {
+    question: "Apa ibu kota Indonesia?",
+    choices: ["Bandung", "Surabaya", "Jakarta", "Medan"],
+    answer: 2
+  },
+  {
+    question: "Siapa presiden pertama Indonesia?",
+    choices: ["Sukarno", "Soeharto", "Habibie", "Jokowi"],
+    answer: 0
+  },
+  {
+    question: "Pulau terbesar di Indonesia?",
+    choices: ["Jawa", "Sumatra", "Kalimantan", "Sulawesi"],
+    answer: 2
+  },
+  {
+    question: "Sungai terpanjang di Indonesia?",
+    choices: ["Kapuas", "Mahakam", "Musik", "Brantas"],
+    answer: 0
+  },
+  {
+    question: "Gunung tertinggi di Indonesia?",
+    choices: ["Semeru", "Rinjani", "Kerinci", "Puncak Jaya"],
+    answer: 3
+  },
+  {
+    question: "Provinsi paling timur di Indonesia?",
+    choices: ["Papua", "Papua Barat", "Maluku", "NTT"],
+    answer: 0
+  },
+  {
+    question: "Tari Kecak berasal dari?",
+    choices: ["Jawa", "Bali", "Sumatera", "Kalimantan"],
+    answer: 1
+  },
+  {
+    question: "Bahasa resmi Indonesia?",
+    choices: ["Jawa", "Melayu", "Indonesia", "Sunda"],
+    answer: 2
+  },
+  {
+    question: "Danau terbesar di Indonesia?",
+    choices: ["Toba", "Singkarak", "Maninjau", "Poso"],
+    answer: 0
+  },
+  {
+    question: "Lambang negara Indonesia adalah?",
+    choices: ["Harimau", "Garuda", "Merak", "Elang"],
+    answer: 1
+  }
+];
